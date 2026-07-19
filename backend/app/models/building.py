94259f334ed7c11d08decd4f24f09ae1bf67c23c
dottomtundu9-MTUNDU <dottomtundu9@gmail.com>
2026-07-19 12:56:24 +0300

    building setup

--- backend/app/models/building.py
+++ backend/app/models/building.py
@@ -8,11 +8,11 @@ class Building(Base):
     __tablename__="buidings"
     id= Column(Integer,primary_key=True,index=True)
     
-    name=Column(String,nullable=True)# LIBRARY
-    code=Column(String,nullable=False,unique=True)#ADM,LIB
+    building_name=Column(String,nullable=True,unique=True)# LIBRARY
+    building_code=Column(String,nullable=False,unique=True)#ADM,LIB
     description=Column(String(255),nullable=False)
     
     created_at=Column(DateTime,default=datetime.now)
     updated_at=Column(DateTime,default=datetime.now,onupdate=datetime.now)
     
-    floor=relationship("Floor",back_populates="buildigs",cascade="all, delete-orphan")
+    #floor=relationship("Floor",back_populates="buildig",cascade="all, delete-orphan")
